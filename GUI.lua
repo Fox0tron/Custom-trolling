1db2b0079d5806ddc1aa0c66d54ca3654cee203b
Fox0tron <164673093+Fox0tron@users.noreply.github.com>
2024-03-24 19:40:05 +0100

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -17,7 +17,7 @@ local OrionLib = {
 			Foxotronic = Color3.fromRGB(195, 130, 9),
 			Second = Color3.fromRGB(0, 0, 0),
 			Stroke = Color3.fromRGB(41, 41, 41),
-			Divider = Color3.fromRGB(60, 60, 60),
+			Divider = Color3.fromRGB(250, 170, 0),
 			Text = Color3.fromRGB(255, 255, 255),
 			TextDark = Color3.fromRGB(150, 150, 150)
 		}
