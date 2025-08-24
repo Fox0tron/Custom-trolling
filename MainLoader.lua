a6548d4b7edb5887a695d38211fae3eb38d45558
Fox0tron <164673093+Fox0tron@users.noreply.github.com>
2025-08-24 18:53:25 +0200

    Create MainLoader.lua

---
+++ MainLoader.lua
@@ -0,0 +1,11 @@
+-- MainLoader.lua
+-- This will load everything in order
+
+-- Load ExploitLibrary.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Fox0tron/Custom-trolling/main/ExploitLibrary.lua"))()
+
+-- Load GUI.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Fox0tron/Custom-trolling/main/GUI.lua"))()
+
+-- Finally load BreakIn.lua (your main script)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Fox0tron/Custom-trolling/main/BreakIn.lua"))()
